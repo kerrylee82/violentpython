0f9fd97b04b5ad95397a93326533c93164f03961
KerryLee82 <kerrylee82@gmail.com>
2016-11-23 23:48:24 +0800

    1st commit, beginning the study. And also will be a utility libraries for the security domain.

--- violentUtil/PassHash.py
+++ violentUtil/PassHash.py
@@ -1,9 +1,11 @@
 import crypt
+from pickle import NONE
+
 
 def getHashedPass(pass, saltstr):
     """'
     return the hashed value with crypt password, but it's not support bcrypt. If bcrypt use getHashedPassWithBcrypt function.
     """
     pass
-def getHashedPassWithBcrypt(pass, saltstr):
+def getHashedPassWithBcrypt(pass, saltstr=NONE):
     pass
