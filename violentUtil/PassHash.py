34e0728b83bb11092de101d37640289167d05e9c
Kerry <Kerry.li@emc.com>
2016-11-25 02:13:41 +0800

    Add something

--- violentUtil/PassHash.py
+++ violentUtil/PassHash.py
@@ -1,11 +1,12 @@
 import crypt
-from pickle import NONE
 
-
-def getHashedPass(pass, saltstr):
-    """'
-    return the hashed value with crypt password, but it's not support bcrypt. If bcrypt use getHashedPassWithBcrypt function.
+def getHashedPass(rawpass, saltstr):
+    """
+    return the hashed value with crypt password, but it's not support bcrypt.
+    If bcrypt use getHashedPassWithBcrypt function.
     """
     pass
-def getHashedPassWithBcrypt(pass, saltstr=NONE):
+
+
+def getHashedPassWithBcrypt(rawpass, saltstr=NONE):
     pass
